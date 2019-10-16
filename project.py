3018106cb3b2a65a127fa5a8f61a723b4992cc8e
damaoooo <damaoooo@163.com>
2019-10-16 23:44:46 +0800

    1016

---
+++ project.py
@@ -0,0 +1,37 @@
+class method():
+    def oddCheck():
+        pass
+    def bytes2Bin():
+        pass
+    def bin2Frames():
+        pass
+    def frames2Bin():
+        pass
+    def bin2Bytes():
+        pass
+    def text2Bytes():
+        pass
+    def bytes2Text():
+        pass
+    def addXorCheck():
+        pass
+class Unit(method):
+    mode = 0
+    local = 0
+    dest = 0
+    def start(self):
+        pass
+    class tcpLayer():
+        def controlCenter():
+            pass
+        def recvFrame():
+            pass
+        def sendFrams():
+            pass
+        def checkXor():
+            pass
+    class dataLayer():
+        def wrapFrame():
+            pass
+        def parseFrame():
+            pass
