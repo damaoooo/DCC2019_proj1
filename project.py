070de85a266ce946d324c0c4cf6022fb2a106254
damaoooo <damaoooo@163.com>
2019-10-17 22:13:40 +0800

    10.17

--- project.py
+++ project.py
@@ -1,10 +1,45 @@
+from os import system
+import time
+from binascii import unhexlify
+
+
+
 class method():
-    def oddCheck(self):
-        pass
-    def bytes2Bin(self):
-        pass
-    def bin2Frames(self):
-        pass
+    def oddCheck(self,checkUnit):
+        rowRes = [0]*8
+        colunmRes = [0]*8
+        for singleByteIndex in range(len(checkUnit)):
+            #row:行  colunm:列
+            listBytes = list(checkUnit[singleByteIndex])
+            for singleBitIndex in range(len(listBytes)):
+                num = int(listBytes[singleBitIndex])
+                rowRes[singleByteIndex]^=num
+                colunmRes[singleBitIndex]^=num
+        return [''.join([str(x) for x in rowRes]),''.join([str(y) for y in colunmRes])]
+            
+    def bytes2Bin(self,Bytes):
+        binres = ''
+        for i in Bytes:
+            binres+=bin(i)[2:].zfill(8)
+        return binres
+    def bin2Frames(self,binText,Framelength):
+        res = []
+        leng = len(binText)
+        tempFrame = []
+        tempFramelen = 0
+        while(leng>0):
+            if(tempFramelen!=Framelength):
+                tempFrame.append(binText[0:8])
+                binText = binText[8:]
+                tempFramelen+=1
+                leng-=8
+            else:
+                res.append(tempFrame)
+                tempFrame=[]
+                tempFramelen=0
+        if(res==[] or tempFrame!=res[-1]):
+            res.append(tempFrame)
+        return res
     def frames2Bin(self):
         pass
     def bin2Bytes(self):
@@ -21,7 +56,7 @@ class Unit(method):
     dest = 0
     def start(self):
         pass
-    class tcpLayer():
+    class tcpLayer(method):
         def controlCenter(self):
             pass
         def recvFrame(self):
@@ -35,3 +70,7 @@ class Unit(method):
             pass
         def parseFrame(self):
             pass
+
+A = method()
+checkUnit = ['11110000','10101010','11111111','00000000','01011010','11001100','00110011','10011001']
+print(A.oddCheck(checkUnit))
